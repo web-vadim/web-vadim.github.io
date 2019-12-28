4674eab9010e1b59754ce2603dee2e66e5259fea
vadim_kravets <kravets4vadim@yahoo.com>
2019-12-28 11:12:51 +0200

    added fotm.html

--- Gromov Studio/js/script.js
+++ Gromov Studio/js/script.js
@@ -3,3 +3,20 @@ document.getElementById('burger').onclick = function() {
     document.getElementById('burg').classList.toggle('active');
     document.getElementsByTagName('body')[0].classList.toggle('lock');
   }
+
+  let inputs = document.querySelectorAll('.input__file');
+  Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+      labelVal = label.querySelector('.label__button').innerText;
+
+    input.addEventListener('change', function (e) {
+      let countFiles = '';
+      if (this.files && this.files.length >= 1)
+        countFiles = this.files.length;
+
+      if (countFiles)
+        label.querySelector('.label__button').innerText = 'Выбрано файлов: ' + countFiles;
+      else
+        label.querySelector('.label__button').innerText = labelVal;
+    });
+  });
