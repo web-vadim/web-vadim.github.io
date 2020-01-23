e7b542746baec4b3b015909bdfe76a8a35fd6b38
vadim_kravets <kravets4vadim@yahoo.com>
2020-01-23 23:12:09 +0200

    added new proj - BigProject

---
+++ BigProjects/js/script.js
@@ -0,0 +1,19 @@
+ 	 document.getElementById('answer-main').onclick = function() {
+    document.getElementById('answer-main').classList.toggle('show');
+    document.getElementById('answer-submain').classList.toggle('show');
+  }
+
+	 document.getElementById('answer-main2').onclick = function() {
+    document.getElementById('answer-main2').classList.toggle('show');
+    document.getElementById('answer-submain2').classList.toggle('show');
+  }
+
+  	 document.getElementById('answer-main3').onclick = function() {
+    document.getElementById('answer-main3').classList.toggle('show');
+    document.getElementById('answer-submain3').classList.toggle('show');
+  }
+
+  	 document.getElementById('answer-main4').onclick = function() {
+    document.getElementById('answer-main4').classList.toggle('show');
+    document.getElementById('answer-submain4').classList.toggle('show');
+  }
